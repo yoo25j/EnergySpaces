606f3be8599b542806c8002a6465866866d57a05
yoo25j <yoo25j@mtholyoke.edu>
2017-11-06 16:03:02 -0500

    First Uploads
    -please download

---
+++ src/MsgProvider.java
@@ -0,0 +1,12 @@
+package exercise1;
+//package edu.mtholyoke.cs.comsc243.kinectUDP;
+
+import java.util.concurrent.BlockingQueue;
+
+public interface MsgProvider {
+	public void start();
+	public void stop();
+	public boolean isRunning();
+	public BlockingQueue<Msg> getMsgQueue();
+
+}
---
+++ src/MultiUserAppDemo.java
@@ -0,0 +1,253 @@
+package exercise1;
+//package edu.mtholyoke.cs.comsc243.kinectUDP;
+
+import java.io.IOException;
+
+import processing.core.PApplet;
+//import processing.core.PShape;
+import processing.core.PVector;
+
+/**
+ * @author eitan
+ *
+ */
+public class MultiUserAppDemo extends PApplet 
+{
+
+	
+	Long firstPersonId = null;
+	Long secondPersonId = null;
+	
+	KinectBodyDataProvider kinectReader;
+	PersonTracker tracker = new PersonTracker();
+	
+	public static float PROJECTOR_RATIO = 1080f/1920.0f;
+
+	
+	/**
+	 * 
+	 * @param useP2D
+	 * @param isFullscreen
+	 * @param windowsScale
+	 */
+	public void createWindow(boolean useP2D, boolean isFullscreen, float windowsScale) 
+	{
+		if (useP2D) 
+		{
+			if(isFullscreen) 
+			{
+				fullScreen(P2D);  			
+			} 
+			else 
+			{
+				size((int)(1920 * windowsScale), (int)(1080 * windowsScale), P2D);
+			}
+		} 
+		else 
+		{
+			if(isFullscreen) 
+			{
+				fullScreen();  			
+			} 
+			else 
+			{
+				size((int)(1920 * windowsScale), (int)(1080 * windowsScale));
+			}
+		}		
+	}
+	
+	// use lower numbers to zoom out (show more of the world)
+	// zoom of 1 means that the window is 2 meters wide and approximately 1 meter tall.
+	public void setScale(float zoom) 
+	{
+		scale(zoom* width/2.0f, zoom * -width/2.0f);
+		translate(1f/zoom , -PROJECTOR_RATIO/zoom );		
+	}
+
+	/**
+	 * 
+	 */
+	public void settings() 
+	{
+		createWindow(false, false, .25f);
+	}
+
+	/**
+	 * 
+	 */
+	public void setup()
+	{
+
+		/*
+		 * use this code to run your PApplet from data recorded by UPDRecorder 
+		 */
+		
+		
+//		try 
+//		{
+//			kinectReader = new KinectBodyDataProvider("test.kinect", 10);
+//		} 
+//		catch (IOException e) 
+//		{
+//			System.out.println("Unable to create kinect producer");
+//		}
+		 
+		
+		kinectReader = new KinectBodyDataProvider(8008);
+
+		kinectReader.start();
+
+	}
+	
+	/**
+	 * 
+	 */
+	public void draw()
+	{
+		setScale(.5f);
+		
+		noStroke();
+
+//		background(200,200,200);
+//		background(0);
+
+//		fill(255,0,0);
+		fill(0, 0, 0, 1);
+		rect(-2,-2,4,4);
+
+		KinectBodyData bodyData = kinectReader.getMostRecentData();
+		tracker.update(bodyData);
+		
+		if(firstPersonId == null && secondPersonId == null) 
+		{
+			if(! tracker.getPeople().isEmpty()) 
+			{
+				for(Long id : tracker.getIds()) 
+				{
+					firstPersonId = id;
+					secondPersonId = id;
+					break; 
+					// we only care about getting one id					
+					// we will arbitrarily use the first in the set
+				}				
+			}
+		}
+		Body person = null;
+		if(tracker.getPeople().containsKey(firstPersonId) && 
+				tracker.getPeople().containsKey(secondPersonId)) 
+		{
+			 person = tracker.getPeople().get(firstPersonId);
+			 person = tracker.getPeople().get(secondPersonId);
+		} 
+		else 
+		{
+			firstPersonId = null;
+			secondPersonId = null;
+		}
+		
+		if(person != null)
+		{
+			PVector head = person.getJoint(Body.HEAD);
+			PVector spine = person.getJoint(Body.SPINE_SHOULDER);
+			PVector spineBase = person.getJoint(Body.SPINE_BASE);
+			PVector shoulderLeft = person.getJoint(Body.SHOULDER_LEFT);
+			PVector shoulderRight = person.getJoint(Body.SHOULDER_RIGHT);
+			PVector footLeft = person.getJoint(Body.FOOT_LEFT);
+			PVector footRight = person.getJoint(Body.FOOT_RIGHT);
+			PVector handLeft = person.getJoint(Body.HAND_LEFT);
+			PVector handRight = person.getJoint(Body.HAND_RIGHT);
+
+
+			fill(255,255,255);
+			noStroke();
+			drawIfValid(head);
+			drawIfValid(spine);
+			drawIfValid(spineBase);
+			drawIfValid(shoulderLeft);
+			drawIfValid(shoulderRight);
+			drawIfValid(footLeft);
+			drawIfValid(footRight);
+			drawIfValid(handLeft);
+			drawIfValid(handRight);
+
+//			if( 
+//					(footRight != null) &&
+//					(footLeft != null) &&
+//				//	(shoulderLeft != null) &&
+//				//	(shoulderRight != null) 
+//					(handLeft != null) &&
+//					(handRight != null) 
+//					) 
+//			{
+//				stroke(255,0,0, 100);
+//				noFill();
+//				strokeWeight(.05f); // because of scale weight needs to be much thinner
+//				curve(
+//						footLeft.x, footLeft.y, 
+//						handLeft.x, handLeft.y, 
+//						handRight.x, handRight.y,
+//						footRight.x, footRight.y
+//						);
+//
+//			}
+			
+			drawShape(handLeft, handRight, footLeft, footRight);
+
+		}
+	}
+
+	/**
+	 * Draws an ellipse in the x,y position of the vector (it ignores z).
+	 * Will do nothing is vec is null.  This is handy because get joint 
+	 * will return null if the joint isn't tracked. 
+	 * @param vec
+	 */
+	public void drawIfValid(PVector vec) 
+	{
+		if(vec != null) 
+		{
+			ellipse(vec.x, vec.y, .1f,.1f);
+		}
+
+	}
+
+
+	/**
+	 * 
+	 * @param p1
+	 * @param p2
+	 * @param p3
+	 * @param p4
+	 */
+	public void drawShape(PVector p1, PVector p2, 
+			PVector p3, PVector p4)
+	{
+		if((p1!= null) && (p2!=null) && (p3 != null) && (p4 !=null)) 
+		{
+//			ellipse(vec.x, vec.y, .1f,.1f);
+			
+			beginShape();
+			fill(56, 34, 255, 50);
+			noStroke();
+//			background(0);
+			vertex(p1.x,p1.y);
+			vertex(p2.x,p2.y);
+			vertex(p3.x, p3.y);
+			vertex(p4.x,p4.y);
+			endShape(CLOSE);
+		}
+		
+		
+	}
+	
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		PApplet.main(MultiUserAppDemo.class.getName());
+	}
+
+}
